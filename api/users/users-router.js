9ccd9574c77b995462be13779b816353bdfce7d3
Winifred Lu <winnie.lu@comcast.net>
2022-02-21 14:51:44 -0500

    basic user router complete

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,7 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
-
+const router = require('express').Router();
+const Users = require('./users-model');
+const { restricted } = require('../auth/auth-middleware');
 /**
   [GET] /api/users
 
@@ -23,6 +24,16 @@
     "message": "You shall not pass!"
   }
  */
+router.get('/', restricted, (req, res, next) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(() => {
+      next();
+    })
+});
 
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
